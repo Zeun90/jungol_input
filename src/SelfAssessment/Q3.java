4e5434a033a054a91c330e44d6a9f825ba250410
Lee JiEun <gsilver190@gmail.com>
2024-06-14 13:13:12 +0900

    자가 진단 3

---
+++ src/SelfAssessment/Q3.java
@@ -0,0 +1,17 @@
+package SelfAssessment;
+
+public class Q3 {
+    public static void main(String[] args) {
+        int num1 = 55;
+        int num2 = 10;
+        int div = num1 - num2;
+
+        System.out.printf("%d - %d = %d\n", num1, num2, div);
+
+        num1 = 2008;
+        num2 = 1999;
+        div = num1 - num2;
+
+        System.out.printf("%d - %d = %d", num1, num2, div);
+    }
+}
