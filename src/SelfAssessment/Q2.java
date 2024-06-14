05ab95d621c0db43a251811bb5284bfd172d8bb4
Lee JiEun <gsilver190@gmail.com>
2024-06-14 10:35:40 +0900

    자가 진단 2

---
+++ src/SelfAssessment/Q2.java
@@ -0,0 +1,12 @@
+package SelfAssessment;
+
+public class Q2 {
+    public static void main(String[] args) {
+        // 정수형 변수 2개 선언하여 값 대입
+        int num1 = -1;
+        int num2 = 100;
+
+        // 출력
+        System.out.printf("%d\n%d", num1, num2);
+    }
+}
