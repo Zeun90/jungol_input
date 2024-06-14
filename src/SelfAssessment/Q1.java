34ffa5f1c24e0050556363e10527a74c07ba6382
Lee JiEun <gsilver190@gmail.com>
2024-06-14 10:28:33 +0900

    자가 진단 1

---
+++ src/SelfAssessment/Q1.java
@@ -0,0 +1,13 @@
+package SelfAssessment;
+
+public class Q1 {
+    public static void main(String[] args) {
+        // 정수형 변수 선언
+        int a;
+
+        // 변수에 값 대입
+        a = -100;
+
+        System.out.println(a);
+    }
+}
