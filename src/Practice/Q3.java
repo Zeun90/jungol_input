0c8b5140a92aee98d62736773a343cb6a564c17d
Lee JiEun <gsilver190@gmail.com>
2024-06-14 13:10:24 +0900

    연습 문제 3

---
+++ src/Practice/Q3.java
@@ -0,0 +1,17 @@
+package Practice;
+
+public class Q3 {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        int sum = num1 + num2;
+
+        System.out.printf("%d + %d = %d\n", num1, num2, sum);
+
+        num1 = 30;
+        num2 = 40;
+        sum = num1 + num2;
+
+        System.out.printf("%d + %d = %d", num1, num2, sum);
+    }
+}
