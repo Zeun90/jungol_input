78eee249fdbe7e952f1b0d3950d7e10d25c14533
Lee JiEun <gsilver190@gmail.com>
2024-06-14 10:25:50 +0900

    연습 문제 1

---
+++ src/Practice/Q1.java
@@ -0,0 +1,16 @@
+package Practice;
+
+public class Q1 {
+    public static void main(String[] args) {
+
+        // 변수 선언
+        int a;
+        char b;
+
+        // 변수에 값 대입
+        a = 10;
+        b = 'A';
+
+        System.out.printf("a = %d\nb = %c", a, b);
+    }
+}
