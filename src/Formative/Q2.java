4d075337bf50dc7dce483c5479f5869196b4f229
Lee JiEun <gsilver190@gmail.com>
2024-06-16 02:40:39 +0900

    형성 평가 2

---
+++ src/Formative/Q2.java
@@ -0,0 +1,11 @@
+package Formative;
+
+public class Q2 {
+    public static void main(String[] args) {
+        double num1 = 80.5;
+        double num2 = 22.34;
+        double num3 = num1 + num2;
+
+        System.out.printf("%10.2f%10.2f%10.2f", num1, num2, num3);
+    }
+}
